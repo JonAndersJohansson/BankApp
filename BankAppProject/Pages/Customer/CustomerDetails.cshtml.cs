bcd8e2462a6b35bd00503e11b21f24600adea799
Jon Johansson <jonjohansson88@gmail.com>
2025-03-28 13:39:17 +0100

    Enhance account management UI and functionality
    - Added success message alert and gender display in CustomerDetails. Changed account creation from a link to a form with a confirmation prompt.
    - Introduced IAccountService in CustomerDetailsModel and added OnPostAddAccountAsync method for handling account creation and redirection with success messaging.

--- BankAppProject/Pages/Customer/CustomerDetails.cshtml.cs
+++ BankAppProject/Pages/Customer/CustomerDetails.cshtml.cs
@@ -1,7 +1,9 @@
 using AutoMapper;
 using BankAppProject.ViewModels;
+using DataAccessLayer.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Services.Account;
 using Services.Customer;
 
 namespace BankAppProject.Pages.Customer
@@ -9,19 +11,19 @@ namespace BankAppProject.Pages.Customer
     public class CustomerDetailsModel : PageModel
     {
         private readonly ICustomerService _customerService;
+        private readonly IAccountService _accountService;
         private readonly IMapper _mapper;
 
+        public CustomerDetailsModel(ICustomerService customerService, IAccountService accountService, IMapper mapper)
+        {
+            _customerService = customerService;
+            _accountService = accountService;
+            _mapper = mapper;
+        }
         public CustomerDetailsViewModel Customer { get; set; } = new();
 
         [BindProperty(SupportsGet = true)]
         public int CustomerId { get; set; }
-
-        public CustomerDetailsModel(ICustomerService customerService, IMapper mapper)
-        {
-            _customerService = customerService;
-            _mapper = mapper;
-        }
-
 
         public async Task<IActionResult> OnGetAsync(int customerId)
         {
@@ -32,6 +34,13 @@ namespace BankAppProject.Pages.Customer
 
             Customer = _mapper.Map<CustomerDetailsViewModel>(customerDto);
             return Page();
+        }
+        public async Task<IActionResult> OnPostAddAccountAsync(int customerId)
+        {
+            await _accountService.CreateAccountAsync(customerId);
+
+            TempData["CreatedAccountMessage"] = $"Account created successfully";
+            return RedirectToPage("/Customer/CustomerDetails", new { customerId = customerId });
         }
     }
 }
